7d0e2321ba356708abd4b9b9784153dbaad30a71
Breyner012 <bhernandezos@cesde.net>
2024-08-17 17:28:33 -0500

    agregue una nueva clase

---
+++ src/notas/pruebagit.java
@@ -0,0 +1,8 @@
+package notas;
+
+public class pruebagit {
+    // al parecer si funciono
+    public static void main(String[] args) {
+        System.out.println("Esto es una prueba para git");
+    }
+}
